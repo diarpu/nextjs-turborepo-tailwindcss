1e79943cf8746e2cdbe6b8d054f9bcb79dd56777
Diego Paz <diegoparancib@gmail.com>
2023-04-08 15:45:32 -0300

    Add first component with custom path

---
+++ apps/web/components/Item.tsx
@@ -0,0 +1,3 @@
+export default function Item() {
+  return <div className='mt-10'>Item</div>
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
+import Item from '@/components/Item'
 import { Button } from 'ui'
 
 export default function Web() {
   return (
-    <div>
+    <div className="bg-diarpu">
       <h1 className="px-2 py-4 text-4xl md:text-5xl">Web</h1>
       <Button />
+      <Item />
     </div>
   )
 }
