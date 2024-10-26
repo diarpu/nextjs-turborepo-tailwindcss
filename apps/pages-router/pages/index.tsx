48cf56bd87a3adeb622113eed65f019a29d9453f
Diarpu <diegoparancib@gmail.com>
2024-10-26 15:18:16 -0300

    update pages-router app

--- apps/pages-router/pages/index.tsx
+++ apps/pages-router/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from 'ui'
 export default function Web() {
   return (
     <div className="bg-diarpu">
-      <h1 className="px-2 py-4 text-4xl md:text-5xl">Web</h1>
+      <h1 className="px-2 py-4 text-4xl md:text-5xl">Pages Router</h1>
       <Button />
       <Item />
     </div>
