a81bde21c7e4bb74b44ef604aa362beec15fec01
Diarpu <diegoparancib@gmail.com>
2024-10-26 14:13:07 -0300

    update config

--- packages/prettier-config-custom/prettier.config.js
+++ packages/prettier-config-custom/prettier.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
   trailingComma: 'none',
   tabWidth: 2,
   semi: false,
   singleQuote: true
 }
+
+module.exports = config
